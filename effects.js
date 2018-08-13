e200ed3ebde0c6d1a1f66ce46120a912952ceb76
Paige Davis <paige.daviana@gmail.com>
2018-08-13 16:45:23 +0000

    looking for ways to improve

---
+++ effects.js
@@ -0,0 +1,21 @@
+function jiffy(jiffy) {
+    document.getElementById(jiffy).innerHTML = "It's not jiffy. It was never jiffy. It was always Jif. I've been fooled. " ;
+    }
+    
+    function pikachu(pikachu) {
+    document.getElementById(pikachu).innerHTML = "So.. Pikachu has an all yellow tail." ;
+    }
+    
+     
+    
+     function mirror(mirror) {
+        document.getElementById(mirror).innerHTML = "Magic mirror on the wall, who's the fairest of them all?" ;
+    }
+    
+     
+    
+     function tootsie (tootsie) {
+    document.getElementById(tootsie).innerHTML = "How many licks does it take to get to the tootsie roll center of a tootsie pop?" ;
+    }
+    
+    
